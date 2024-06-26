bed7a853e08274a21dddd7397b281f5cb621ec8d
mjekrami <mjavad.ekrami@gmail.com>
2024-06-27 02:56:33 +0330

    added first section and some data

---
+++ components/Grid.tsx
@@ -0,0 +1,20 @@
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
+const Grid = () => {
+  return (
+    <section>
+      <BentoGrid>
+        {gridItems.map((item) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+export default Grid;
