b84288972080335803e1c2531c5ea5954ba3a9aa
mjekrami <mjavad.ekrami@gmail.com>
2024-06-27 19:03:43 +0330

    completed hero section

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -4,15 +4,30 @@ const Grid = () => {
   return (
     <section>
       <BentoGrid>
-        {gridItems.map((item) => (
-          <BentoGridItem
-            id={item.id}
-            key={item.id}
-            title={item.title}
-            description={item.description}
-            className={item.className}
-          />
-        ))}
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          ),
+        )}
       </BentoGrid>
     </section>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,7 +28,7 @@ const Hero = () => {
           </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Turning Ideas Into Reliable Infrastructure"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Mohammad Javad, Devops Engineer Based in Iran{" "}
